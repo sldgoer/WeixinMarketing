750c21872d083db6a5debe98a6c5fddd7b6eb38f
Ben <Ben@DESKTOP-L61HVFP>
2017-07-27 18:28:41 +0800

    添加项目文件。

---
+++ Senparc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Senparc.Web.Startup))]
+namespace Senparc.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
